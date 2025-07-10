b4adef919ec9f6fea24991e65dbb7fe840e4a2af
chilakamarthinavya <chilakamarthinavya@gmail.com>
2025-07-10 14:25:48 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+
+variable "zone_id" {
+    default = "Z06155041CMCI8FN4RFWT"
+  
+}
+
+variable "zone_name" {
+    default = "latha.site"
+  
+}
+
+variable "component" {
+
+  
+}
+
+variable "rule_priority" {
+  
+}
